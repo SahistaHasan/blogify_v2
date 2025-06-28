9cc65254ea82219e687e3bb1082bcc1f82ccbc5f
Sahista Hasan <sahistahasan001@gmail.com>
2025-06-28 19:13:01 +0530

    cors enable

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ const app=express();
 
 await connectDB();
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'https://blogify-5naqs0qme-blogifys-projects-fd54692c.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000;
